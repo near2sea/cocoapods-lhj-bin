2cd3aaa8ba1e468c8662674da54a618d95e83f09
lihaijian <sanan.li@qq.com>
2021-05-12 10:37:47 +0800

    生成国际化文件

--- lib/cocoapods-lhj-bin/command/bin/local.rb
+++ lib/cocoapods-lhj-bin/command/bin/local.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
 require 'csv'
 
 module Pod
   class Command
     class Bin < Command
       class Local < Bin
-        self.summary = '国际化文件变更'
+        self.summary = '生成国际化文件'
 
         def initialize(argv)
           @current_path = argv.shift_argument || Dir.pwd
@@ -18,12 +19,25 @@ module Pod
           write_zh_hans_strings
         end
 
-        def read_csv_file
-          path = "#{@current_path}/b_lvr_land_a_land.csv"
-          CSV.foreach(path) { |row| @key_map[row[0]] = { zh: row[1], en: row[3] } unless row[0] =~ /[\u4e00-\u9fa5]/ }
+        def en_dir_name
+          'en.lproj'
         end
 
-        def file_str(type)
+        def zh_dir_name
+          'zh-Hant.lproj'
+        end
+
+        def generate_file_name
+          'Localizable.strings'
+        end
+
+        def read_csv_file
+          Dir.glob("#{@current_path}/**/*.csv").each do |p|
+            CSV.foreach(p) { |row| @key_map[row[0]] = { zh: row[1], en: row[3] } unless row[0] =~ /[\u4e00-\u9fa5]/ }
+          end
+        end
+
+        def format_str(type)
           str = ''
           @key_map.each do |k, v|
             str += "\"#{k}\" = \"#{v[type]}\";\n"
@@ -33,19 +47,20 @@ module Pod
 
         def write_to_file(file, type)
           FileUtils.rm_rf(file) if File.exist?(file)
+          FileUtils.mkdir_p(File.dirname(file)) unless File.exist?(File.dirname(file))
           File.open(file, 'w+') do |f|
-            str = file_str(type)
+            str = format_str(type)
             f.write(str)
           end
         end
 
         def write_en_strings
-          file = "#{@current_path}/Main_en.strings"
+          file = File.join(@current_path, en_dir_name, generate_file_name)
           write_to_file(file, :en)
         end
 
         def write_zh_hans_strings
-          file = "#{@current_path}/Main_zh_hans.strings"
+          file = File.join(@current_path, zh_dir_name, generate_file_name)
           write_to_file(file, :zh)
         end
 
