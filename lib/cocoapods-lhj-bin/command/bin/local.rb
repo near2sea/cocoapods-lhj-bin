82f159ec02e0b03b98e0d51c481eb1f79a97973c
lihaijian <sanan.li@qq.com>
2021-05-12 18:39:28 +0800

    国际化

--- lib/cocoapods-lhj-bin/command/bin/local.rb
+++ lib/cocoapods-lhj-bin/command/bin/local.rb
@@ -10,10 +10,13 @@ module Pod
         def initialize(argv)
           @current_path = argv.shift_argument || Dir.pwd
           @key_map = {}
+          @trans_map = {}
+          @trans_map_invert = {}
           super
         end
 
         def run
+          load_trans_map
           read_csv_file
           write_en_strings
           write_zh_hans_strings
@@ -35,13 +38,26 @@ module Pod
           'Localizable.strings'
         end
 
-        def load_yaml_from_remote
+        def yaml_file
+          File.join(Pod::Config.instance.home_dir, 'zh2hant.yml')
+        end
+
+        def load_trans_map
+          require 'yaml'
+          down_load_yaml unless File.exist?(yaml_file)
+          contents = YAML.safe_load(File.open(yaml_file))
+          @trans_map = contents.to_hash
+          @trans_map_invert = @trans_map.invert
+        end
+
+        def down_load_yaml
           require 'open-uri'
-          key_map = {}
-          URI.open('http://aomi-ios-repo.oss-cn-shenzhen.aliyuncs.com/zh2hant.yml') do |f|
-            key_map = YAML.safe_load(f.read)
+          UI.puts "开始下载简繁配置文件...\n"
+          URI.open('http://aomi-ios-repo.oss-cn-shenzhen.aliyuncs.com/zh2hant.yml') do |i|
+            File.open(yaml_file, 'w+') do |f|
+              f.write(i.read)
+            end
           end
-          puts key_map
         end
 
         def read_csv_file
@@ -53,7 +69,9 @@ module Pod
         def format_str(type)
           str = ''
           @key_map.each do |k, v|
-            str += "\"#{k}\" = \"#{v[type]}\";\n"
+            reg = /#{@trans_map_invert.keys}/
+            val = v[type].gsub(reg) { |s| @trans_map_invert[s] }
+            str += "\"#{k}\" = \"#{val}\";\n"
           end
           str
         end
